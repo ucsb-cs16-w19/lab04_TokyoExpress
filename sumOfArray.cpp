3f540af5872ae675e9c1571057cf0b80b692e03e
TokyoExpress <samuelzguo@ucsb.edu>
2019-02-12 15:58:23 -0800

    Final

--- sumOfArray.cpp
+++ sumOfArray.cpp
@@ -1,5 +1,11 @@
 #include "arrayFuncs.h"
 
 int sumOfArray(int a[], int size) {
-  return 0;
+  // variable for counting
+  int sum = 0;
+  // array parser
+  for(int i = 0; i < size; i++) {
+    sum += a[i];
+  }
+  return sum;
 }
