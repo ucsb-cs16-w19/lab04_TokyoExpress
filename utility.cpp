4d80d7d97ad7ddcd9dd421a9930321ca9996d66a
TokyoExpress <samuelzguo@ucsb.edu>
2019-02-12 12:18:28 -0800

    implemented utility function - <prime, odd, even>

--- utility.cpp
+++ utility.cpp
@@ -1,21 +1,19 @@
 // utility.cpp
 
-// IN THIS FILE, define any of your OWN functions you may need to 
-// solve the problems.    
-
-// For example, if you need an isPrime function, you can put the function
-// definition in this file.  Similarly, isOdd or isEven might be useful.
-
-// You will need to include the function prototype in "utility.h" and
-// then be sure to  #include "utility.h" in the file where you use
-// these functions
-
-bool isOdd(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+bool isOdd(int x) {
+  // simple mod function
+  if(x % 2 == 1) return true;
+  return false; 
 }
-bool isEven(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+bool isEven(int x) {
+  // another simple mod function
+  if(x % 2 == 0) return true;
+  return false;  
 }
-bool isPrime(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+bool isPrime(int x) {
+  // a mod check for every number between 2 and x
+  for(int i = 2; i < x; i++) {
+    if(x % i == 0) return false;
+  }
+  return true;
 }
