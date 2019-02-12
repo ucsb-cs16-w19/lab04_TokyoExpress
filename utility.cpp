00c93a9db075f552df4cce3bfd64985b713e5a02
TokyoExpress <samuelzguo@ucsb.edu>
2019-02-12 12:22:48 -0800

    all tests pass

--- utility.cpp
+++ utility.cpp
@@ -2,7 +2,7 @@
 
 bool isOdd(int x) {
   // simple mod function
-  if(x % 2 == 1) return true;
+  if(x % 2 == 1 || x % 2 == -1) return true;
   return false; 
 }
 bool isEven(int x) {
@@ -11,6 +11,10 @@ bool isEven(int x) {
   return false;  
 }
 bool isPrime(int x) {
+  // negative check
+  if(x < 0) return false;
+  // fringe cases check
+  if(x == 1 || x == 0) return false;
   // a mod check for every number between 2 and x
   for(int i = 2; i < x; i++) {
     if(x % i == 0) return false;
